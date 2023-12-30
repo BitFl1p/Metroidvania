1cf9c6f2d198eaaf38c34794653a5282c2e36d93
Bit <jumiciobi@gmail.com>
2023-12-30 18:47:54 +0000

    build

--- Assets/Scripts/Misc/Mouse.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Mouse : MonoBehaviour
-{
-    private void Update() => transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    
-}
--- Assets/Scripts/Misc/StayTheFuckWhereYoureSupposedToBe.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StayTheFuckWhereYoureSupposedToBe : MonoBehaviour
-{
-    public Vector2 offset = Vector2.zero;
-    void Update() => transform.localPosition = offset;
-}
